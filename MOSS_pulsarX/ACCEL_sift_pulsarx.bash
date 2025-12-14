2e0fe5577b26a1aaeeb2c278da4e5269c9cb1471
Dejiang Yin <dj.yin@foxmail.com>
2025-12-14 17:36:35 +0800

    Update ACCEL_sift_pulsarx.bash

--- MOSS_pulsarX/ACCEL_sift_pulsarx.bash
+++ MOSS_pulsarX/ACCEL_sift_pulsarx.bash
@@ -17,7 +17,7 @@ zmax=_ACCEL_20
 ACCEL_sift_pulsarx=/home/data/NGC6517/20190625/demo/pulsarX_sifting/ACCEL_sift_pulsarx.py
 fast_fold.template=/home/software/PulsarX/include/template/fast_fold.template
 # Only accel:
-python ""${ACCEL_sift_pulsarx}"" -ACCEL 20 -minP 2. -maxP 6.8 -rDM 5 -minS 1 -maxS 40 2> pulsarX_cands.List
+python ""${ACCEL_sift_pulsarx}"" -ACCEL 20 -minP 2. -maxP 6.8 -minS 1 -maxS 40 2> pulsarX_cands.List
 # For jerk search:
 #python ""${python_1}"" -ACCEL 300 -JERK 900 -minP 2. -maxP 10.8 -rDM 4 -minS 6 -maxS 40 2> pulsarX_cands.List
 
